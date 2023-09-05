535529ed4507c236b423760b1cb71f2010a98909
emanuelefarelli <emaaliens888@gmail.com>
2023-09-05 15:10:58 +0200

    Added basic structure to vue/vite project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue';
+</script>
+
+<template>
+<AppHeader/>
+<AppMain/>
+</template>
+
+<style lang="scss">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<template>
+<main>
+    <h1>
+        Main
+    </h1>
+</main>
+</template>
+
+<script>
+export default{
+    name: 'AppMain'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
