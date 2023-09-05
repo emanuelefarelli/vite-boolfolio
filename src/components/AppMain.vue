3c3d55f3915bb30f061af81f794df46290dfa73d
emanuelefarelli <emaaliens888@gmail.com>
2023-09-05 15:42:09 +0200

    Made axios call and printed data on console

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,9 +7,36 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default{
-    name: 'AppMain'
+    name: 'AppMain',
+    data(){
+        return{
+            projects:[],
+            apiUrl:'http://127.0.0.1:8000/api/projects'
+        }
+    },
+    methods:{
+        getProjects(){
+            axios.get(this.apiUrl, {
+                params: {}
+            })
+                .then( (response) => {
+                    // console.log(response.data);
+                    this.projects = response.data;
+                    console.log(this.projects);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+    created(){
+        this.getProjects();
+    }
 }
+
 </script>
 
 <style scoped lang="scss">
